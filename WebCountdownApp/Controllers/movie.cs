4a22f20dfb88a96024eb81e646d6772f263dfdd9
Carmen Long <carmen.long@hobbylobby.com>
2017-05-23 16:22:04 -0500

    how do I work with the view

---
+++ WebCountdownApp/Controllers/movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCountdownApp.Controllers
+{
+    public class Movie
+    {
+        public string Event { get; set; }
+        public string Date { get; set; }
+    }
+}
