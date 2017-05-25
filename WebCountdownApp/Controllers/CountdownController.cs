d7b54bc8e23d453f0fd1e02c54761d9e1d18f5dd
Carmen Long <carmen.long@hobbylobby.com>
2017-05-25 16:45:00 -0500

    need to display to screen && call on interval

--- WebCountdownApp/Controllers/CountdownController.cs
+++ WebCountdownApp/Controllers/CountdownController.cs
@@ -19,12 +19,26 @@ namespace WebCountdownApp.Controllers
                 {
                     movie_data = JsonConvert.DeserializeObject<Movie>(sr.ReadToEnd());
                 }
-                    return Content(movie_data.Event + " will arrive in " + movie_data.Date);
+                string movie_name = movie_data.Event;
+                string howLong = HoursRemaining(movie_data.Date);
+                return Content(movie_name + " will arrive in " + howLong);
             }
             catch(Exception err)
             {
                 throw new Exception("Everything is broken.", err);
             }
+        }
+
+        public static string HoursRemaining(string day)
+        {
+            DateTime dt = Convert.ToDateTime(day);
+            DateTime now = DateTime.Now;
+            var diffInMinutes = (dt - now).TotalMinutes;
+            var diffInHours = diffInMinutes / 60;
+            var appendMinutes = diffInMinutes % 60;
+            string hours = Math.Truncate(diffInHours).ToString() + " hours";
+            string minutes = appendMinutes > 0 ? " and " + Math.Truncate(appendMinutes).ToString() + " minutes" : "";
+            return hours + minutes;
         }
 
     }
